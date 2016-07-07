af40c13bd6f840defe1a81a336c7bc2fe3c53521
hastalavistababy <lortkipanidzemogeli@gmail.com>
2016-07-07 17:00:30 +0400

    Initial commit.

---
+++ textShake.js
@@ -0,0 +1,52 @@
+/**
+ * @name       Textshake
+ * @author     Mogeli lortkipanidze <lortkipanidzemogeli@gmail.com>
+ * @version    0.1.1
+ *
+ * @requires   jQuery
+ *
+ * Copyright (c) 2016 by Mogeli lortkipanidze
+ */
+ 
+(function($){
+	$.fn.textShake = function(time, step,  maintext, shaketext, afteraction){
+		var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+			el = $(this),
+
+		shakeInterval = setInterval(function(){
+
+			time -= step;
+			var element;
+			if(time - step > 0) {
+				element = maintext.split("");
+				for (var l = 0; l < element.length; l++) {
+					element[l].match(/[a-z]/gi) && (element[l] = alphabet.charAt(Math.floor(Math.random() * alphabet.length)));
+				}
+				el.text(element.join(""));
+			}
+			else if(0 >= time - step && time > 0) {
+				element = maintext.split("");
+				for (var c = shaketext.split(""), d = 0; d < element.length; d++) {
+					if(d % 2 === 0) {
+						element[d];
+					}
+					else {
+						c[d];
+					}
+				}
+				el.text(element.join(""));
+			}
+			else {
+				if(0 >=time) {
+					clearInterval(shakeInterval);
+					el.text(shaketext);
+					if(typeof afteraction == "function") {
+						afteraction();
+					}
+				}
+			}
+
+
+		}, step);
+	}
+}( jQuery ));
